2c3ec4317370621f2b2e57a9d608267e86e01077
Luka Barth <luka.costa@db1.com.br>
2025-04-02 17:23:13 -0300

    add security layer in file upload

--- src/Controller/NewVideoController.php
+++ src/Controller/NewVideoController.php
@@ -6,6 +6,7 @@ namespace Alura\Mvc\Controller;
 
 use Alura\Mvc\Entity\Video;
 use Alura\Mvc\Repository\VideoRepository;
+use finfo;
 
 class NewVideoController implements Controller
 {
@@ -26,14 +27,20 @@ class NewVideoController implements Controller
 
         $video = new Video($url, $titulo);
         if ($_FILES['image']['error'] === UPLOAD_ERR_OK) {
-            move_uploaded_file(
-                $_FILES['image']['tmp_name'],
-                __DIR__ . '/../../public/img/uploads/' . $_FILES['image']['name']
-            );
-            $video->setFilePath($_FILES['image']['name']);
+            $safeFileName = uniqid('upload_') . pathinfo($_FILES['image']['name'], PATHINFO_BASENAME);
+            $finfo = new finfo(FILEINFO_MIME_TYPE);
+            $mimeType = $finfo->file($_FILES['image']['tmp_name']);
+
+            if (str_starts_with($mimeType, 'image/')) {
+                move_uploaded_file(
+                    $_FILES['image']['tmp_name'],
+                    __DIR__ . '/../../public/img/uploads/' . $safeFileName
+                );
+                $video->setFilePath($safeFileName);
+            }
         }
 
-        $success = $this->videoRepository->add(new Video($url, $titulo));
+        $success = $this->videoRepository->add($video);
         if ($success === false) {
             header('Location: /?sucesso=0');
         } else {
