089dfab330b07a14e088977b008bf2c84168803b
Luka Barth <luka.costa@db1.com.br>
2025-04-02 12:43:47 -0300

    module 2

---
+++ public/index.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+use Alura\Mvc\Controller\{
+    Controller,
+    DeleteVideoController,
+    EditVideoController,
+    Error404Controller,
+    NewVideoController,
+    VideoFormController,
+    VideoListController
+};
+use Alura\Mvc\Repository\VideoRepository;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$dbPath = __DIR__ . '/../banco.sqlite';
+$pdo = new PDO("sqlite:$dbPath");
+$videoRepository = new VideoRepository($pdo);
+
+$routes = require_once __DIR__ . '/../config/routes.php';
+
+$pathInfo = $_SERVER['PATH_INFO'] ?? '/';
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+
+session_start();
+$isLoginRoute = $pathInfo === '/login';
+if (!array_key_exists('logado', $_SESSION) && !$isLoginRoute) {
+    header('Location: /login');
+    return;
+}
+
+$key = "$httpMethod|$pathInfo";
+if (array_key_exists($key, $routes)) {
+    $controllerClass = $routes["$httpMethod|$pathInfo"];
+
+    $controller = new $controllerClass($videoRepository);
+} else {
+    $controller = new Error404Controller();
+}
+
+$controller->processaRequisicao();
